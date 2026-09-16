eccaf9f85759ce9bbfc136d4ee606f43986aabda
Aditi sharma <aditisharma040905@gmail.com>
2026-09-16 23:07:17 +0530

    Time: 27 ms (75.45%), Space: 14 MB (49.62%) - LeetHub

---
+++ 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def replaceElements(self, arr):
+        """
+        :type arr: List[int]
+        :rtype: List[int]
+        """
+        max_right = -1
+
+        for i in range(len(arr) - 1, -1, -1):
+            current = arr[i]
+            arr[i] = max_right
+
+            if current > max_right:
+                max_right = current
+
+        return arr
+        
