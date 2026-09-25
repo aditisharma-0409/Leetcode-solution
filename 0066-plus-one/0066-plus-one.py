a777b82240eff681506a0b3b12caa5c9aded8178
Aditi sharma <aditisharma040905@gmail.com>
2026-09-25 21:20:15 +0530

    Time: 0 ms (100%), Space: 12.2 MB (90.15%) - LeetHub

--- 0066-plus-one/0066-plus-one.py
+++ 0066-plus-one/0066-plus-one.py
@@ -5,10 +5,9 @@ class Solution(object):
         :rtype: List[int]
         """
         for i in range(len(digits)-1,-1,-1):
-            if digits[i]< 9:
+            if digits[i]<9:
                 digits[i]+=1
                 return digits
-            
             digits[i]=0
         digits.insert(0,1)
         return digits
